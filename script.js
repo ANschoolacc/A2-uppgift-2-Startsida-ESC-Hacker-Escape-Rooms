fcdc6b94c141daca0aaf23ad3d7aec3db04cf0f5
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2023-11-08 17:48:34 +0100

    Added comments and changed settimeout to 130ms

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+/*Eventlistener that reacts to click on menu lines in mobilemode */
 document.querySelector(".topCont__menuBtn").addEventListener("click", () => {
+  /*Waits 130 ms and then adds class to element */
   setTimeout(() => {
     document.querySelector(".menuCont").classList.add("menuCont--open");
-  }, 150);
+  }, 130);
 
+  /*Adds class to element */
   document.querySelector(".body").classList.add("body--openMenu");
-
+  /*Adds class to element */
   document.querySelector(".fadeWhite").classList.add("fadeWhite--active");
 });
-
+/*Eventlistener that reacts to the X symbol in overlaymenu */
 document.querySelector(".menuCont__close").addEventListener("click", () => {
+  /*Removes class from element */
   document.querySelector(".menuCont").classList.remove("menuCont--open");
-
+  /*Removes class from element */
   document.querySelector(".body").classList.remove("body--openMenu");
-
+  /*Removes class from element */
   document.querySelector(".fadeWhite").classList.remove("fadeWhite--active");
 });
