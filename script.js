d722f834947f81bd83269917c02d880a2a20732b
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2023-11-10 10:35:47 +0100

    changed selector names in functions

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 /*Eventlistener that reacts to click on menu lines in mobilemode */
-document.querySelector(".topCont__menuBtn").addEventListener("click", () => {
-  /*Waits 130 ms and then adds class to element */
-  setTimeout(() => {
-    document.querySelector(".menuCont").classList.add("menuCont--open");
-  }, 130);
+document
+  .querySelector(".logoAndTitle__hamburgerBtn")
+  .addEventListener("click", () => {
+    /*Waits 130 ms and then adds class to element */
+    setTimeout(() => {
+      document
+        .querySelector(".hamburgerMenu__menuCont")
+        .classList.add("hamburgerMenu__menuCont--open");
+    }, 130);
 
-  /*Adds class to element */
-  document.querySelector(".body").classList.add("body--openMenu");
-  /*Adds class to element */
-  document.querySelector(".fadeWhite").classList.add("fadeWhite--active");
+    /*Adds class to element */
+    document.querySelector(".body").classList.add("body--openMenu");
+    /*Adds class to element */
+    document
+      .querySelector(".hamburgerMenu")
+      .classList.add("hamburgerMenu--active");
 
-  /*Eventlistener that reacts to the X symbol in overlaymenu */
-  document.querySelector(".menuCont__close").addEventListener("click", () => {
-    /*Removes class from element */
-    document.querySelector(".menuCont").classList.remove("menuCont--open");
-    /*Removes class from element */
-    document.querySelector(".body").classList.remove("body--openMenu");
-    /*Removes class from element */
-    document.querySelector(".fadeWhite").classList.remove("fadeWhite--active");
+    /*Eventlistener that reacts to the X symbol in overlaymenu */
+    document
+      .querySelector(".hamburgerMenu__closeBtn")
+      .addEventListener("click", () => {
+        /*Removes class from element */
+        document
+          .querySelector(".hamburgerMenu__menuCont")
+          .classList.remove("hamburgerMenu__menuCont--open");
+        /*Removes class from element */
+        document.querySelector(".body").classList.remove("body--openMenu");
+        /*Removes class from element */
+        document
+          .querySelector(".hamburgerMenu")
+          .classList.remove("hamburgerMenu--active");
+      });
   });
-});
